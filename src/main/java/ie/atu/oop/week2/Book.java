14c73867b4b2b80268e5ab1002a16ebd158e493b
ThiagoAlves-STUDENT <G00478189@atu.ie>
2026-09-23 16:29:19 +0100

    Created new book class and created an instance in main

---
+++ src/main/java/ie/atu/oop/week2/Book.java
@@ -0,0 +1,9 @@
+package ie.atu.oop.week2;
+
+public class Book {
+    public String title;
+    public String author;
+    public int paegcount;
+    public boolean available = true;
+    }
+
--- src/main/java/ie/atu/oop/week2/Main.java
+++ src/main/java/ie/atu/oop/week2/Main.java
@@ -6,5 +6,15 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("hello OOP");
+
+        Book firstBook = new Book();
+        firstBook.title = "dune";
+        firstBook.author = "frank herbert";
+        firstBook.paegcount = 412;
+
+        System.out.println("this Book is " + firstBook.title);
+        System.out.println("the author of this book is " + firstBook.author);
+        System.out.println("the page count of this book is " + firstBook.paegcount);
+        System.out.println("the status of this book is " + firstBook.available);
     }
 }
